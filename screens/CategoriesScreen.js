9c35774f95ab4d4c13ab5a257498d0c451ba7136
saniok333 <alex.o.csc@gmail.com>
2022-08-18 16:19:54 +0300

    Implementing Navigation Between Two Screens

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -3,14 +3,20 @@ import CategoryGridTile from '../components/CategoryGridTile';
 
 import { CATEGORIES } from '../data/dummy-data';
 
-const renderCategoryItem = (itemData) => {
-  const {
-    item: { title, color },
-  } = itemData;
-  return <CategoryGridTile title={title} color={color} />;
-};
+const CategoriesScreen = ({ navigation }) => {
+  const renderCategoryItem = (itemData) => {
+    const {
+      item: { title, color },
+    } = itemData;
 
-const CategoriesScreen = () => {
+    const onPressHandler = () => {
+      navigation.navigate('MealsOverview');
+    };
+
+    return (
+      <CategoryGridTile title={title} color={color} onPress={onPressHandler} />
+    );
+  };
   return (
     <FlatList
       data={CATEGORIES}
