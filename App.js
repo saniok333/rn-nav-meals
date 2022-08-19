093cbd6b1154de6a48b5747abe12ec4bdacbff83
saniok333 <alex.o.csc@gmail.com>
2022-08-19 10:57:24 +0300

    Default Headers & Backgrounds styling for Navigator

--- App.js
+++ App.js
@@ -13,17 +13,21 @@ export default function App() {
     <>
       <StatusBar style="light" />
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="MealsCategories">
+        <Stack.Navigator
+          initialRouteName="MealsCategories"
+          screenOptions={{
+            headerStyle: { backgroundColor: '#351401' },
+            headerTintColor: 'white',
+            contentStyle: {
+              backgroundColor: '#3f2f25',
+            },
+          }}
+        >
           <Stack.Screen
             name="MealsCategories"
             component={CategoriesScreen}
             options={{
               title: 'All Categories',
-              headerStyle: { backgroundColor: '#351401' },
-              headerTintColor: 'white',
-              contentStyle: {
-                backgroundColor: '#3f2f25',
-              },
             }}
           />
           <Stack.Screen name="MealsOverview" component={MealsOverviewScreen} />
